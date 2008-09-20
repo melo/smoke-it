e013d17005cd54d4dd9932bf5a438e4c5b56abe8
Pedro Melo <melo@simplicidade.org>
2008-09-20 17:43:40 +0100

    Added initial skel for App::Smolder::Report module
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ App-Smolder-Report/lib/App/Smolder/Report.pm
@@ -0,0 +1,107 @@
+package App::Smolder::Report;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+App::Smolder::Report - The great new App::Smolder::Report!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::Smolder::Report;
+
+    my $foo = App::Smolder::Report->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-smolder-report at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Smolder-Report>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::Smolder::Report
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Smolder-Report>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Smolder-Report>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Smolder-Report>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Smolder-Report>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of App::Smolder::Report
